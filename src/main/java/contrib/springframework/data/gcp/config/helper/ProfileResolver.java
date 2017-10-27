51d732fdd2a1c59a78f2bd5a0f2e5b08199fe14c
Matt Byrne <matt@3wks.com.au>
2017-10-27 14:38:29 +1100

    Config utility to activate profiles automatically for local and gcp deployed environments.

---
+++ src/main/java/contrib/springframework/data/gcp/config/helper/ProfileResolver.java
@@ -0,0 +1,51 @@
+package contrib.springframework.data.gcp.config.helper;
+
+import com.google.appengine.api.utils.SystemProperty;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+public class ProfileResolver {
+    private List<Function<String, String>> extractorFunctions = new ArrayList<>();
+
+    /**
+     * Extract additional profile name from the {@link SystemProperty#applicationId} when the
+     * environment is not local development.
+     *
+     * @param extractorFunction Function used to retrieve the additional profile value.
+     *
+     * @return this
+     */
+    public ProfileResolver setAdditionalProfileExtractor(Function<String, String> extractorFunction) {
+        this.extractorFunctions.add(extractorFunction);
+        return this;
+    }
+
+    public List<String> getProfiles() {
+        List<String> profiles = new ArrayList<>();
+
+        if (isProduction()) {
+            String applicationId = SystemProperty.applicationId.get();
+            if (StringUtils.isNotBlank(applicationId)) {
+                for (Function<String, String> extractorFunction : extractorFunctions) {
+                    String extracted = extractorFunction.apply(applicationId);
+                    if (StringUtils.isNotBlank(extracted)) {
+                        profiles.add(extracted);
+                    }
+                }
+                // We want the original applicationId to be the winning profile as it should be more specific
+                profiles.add(applicationId);
+            }
+        } else {
+            profiles.add("local");
+        }
+        return profiles;
+    }
+
+    private boolean isProduction() {
+        return Objects.equals(SystemProperty.Environment.Value.Production.name(), SystemProperty.Environment.environment.get());
+    }
+}
---
+++ src/test/java/contrib/springframework/data/gcp/config/helper/ProfileExtractorsTest.java
@@ -0,0 +1,29 @@
+package contrib.springframework.data.gcp.config.helper;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfileExtractorsTest {
+
+    @Test
+    public void afterLastDash() {
+        String result = ProfileExtractors.AFTER_LAST_DASH.apply("lots-and-lots-of-dashes");
+
+        assertThat(result, is("dashes"));
+    }
+
+    @Test
+    public void afterLastDash_willReturnBlank_whenNoDashes() {
+        String result = ProfileExtractors.AFTER_LAST_DASH.apply("nodashes");
+
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void staticValue_willReturnInpuValue() {
+        assertThat(ProfileExtractors.staticValue("server").apply("anything"), is("server"));
+    }
+
+}
